2c3b91ace69f92cd0bdea03daa260b1bde34f5ea
LaraGabriella <gabriellalara479@gmail.com>
2024-05-06 17:39:20 -0300

    segundo commit

--- button3.py
+++ button3.py
@@ -1,14 +1,11 @@
 from kivy.app import App 
 from kivy.uix.button import Button 
-
-def botao_pressionado(instance): 
-    print("Botão pressionado!") 
+from kivy.utils import get_color_from_hex 
 
 class MinhaApp(App):
     def build(self): 
-        btn = Button(text="Clique Aqui")
-        btn.bind(on_press=botao_pressionado) 
-        return btn 
-    
-if __name__ == "__main__": 
-     MinhaApp().run()
+        return Button(text='Clique Aqui', background_color=get_color_from_hex('#3498db'))
+                      
+            
+if __name__ == "__main__":
+    MinhaApp().run()
--- label7.py
+++ label7.py
@@ -7,7 +7,7 @@ class RotuloApp(App):
         layout = BoxLayout(orientation="vertical") 
         self.lab1 = Label(text="SENAI", color=[1, 0, 0, 1],font_size=40, bold=True) 
 
-        self.lab2 = Label(text="SESI", color=[0, 1, 0 1],font_size=40, italic=True) 
+        self.lab2 = Label(text="SESI", color=[0, 1, 0, 1],font_size=40, italic=True) 
 
         self.lab3 = Label(text="ENEM", color=[0, 0, 1, 1], font_size=40, font_name="Arial",underline=True) 
 
