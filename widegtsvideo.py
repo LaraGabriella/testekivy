20767aeba4b9253611aeceb9b1642c60c7eac4d3
LaraGabriella <gabriellalara479@gmail.com>
2024-05-07 17:34:37 -0300

    terceiro commit

---
+++ widegtsvideo.py
@@ -0,0 +1,12 @@
+from kivy.app import App 
+from kivy.uix.video import Video 
+
+class MinhaApp(App): 
+    def build(self): 
+        return Video(source="caminho/para/deu/video.mp4") 
+    
+if __name__ == "__main__": 
+    MinhaApp().run()
+
+
+
